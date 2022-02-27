1807e73c2663f8302126bdaecd8731e719d43b24
james <Apodisation13@gmail.com>
2022-02-27 13:35:10 +0300

    card modal try 1

--- src/components/HandComp.vue
+++ src/components/HandComp.vue
@@ -1,57 +1,25 @@
 <template>
-<div class="hand" >
+<div class="hand"
+
+>
 
   <div class="card" :style="border(card, index)"
-  v-for="(card, index) in hand" :key='index'>
+    v-for="(card, index) in hand" :key='card'
+    @dblclick="chose_player_card(index)"
+  >
 
-    <div 
-      @contextmenu.prevent 
-      @click.right="i=index; show_card_modal=true" @mouseleave="show_card_modal=false"
-      @click="chose_player_card(index)"
-      >
-    
-    <img class="img" :src="card.image" v-if="card.image" alt="">
-      
-    <div class="typee" v-if="card.type === 'Special'">
-      <span>&starf;</span>
-    </div>
-    
-    <div class="diamond" :style="background_color(card)">
-      <span3>&dagger;{{ card.damage }}</span3>
-    </div>
-    
-    <div class="circle" :style="{'backgroundColor': 'orange'}"
-    v-if="card.ability.name === 'damage-all'">
-      <span>&#9850;</span>
-    </div>
-    <div class="circle" :style="{'backgroundColor': 'green'}"
-    v-if="card.ability.name === 'heal'">
-      <span>+&hearts;{{ card.heal }}</span>
-    </div>
-    
-    <div class="charges">
-      <span>{{ card.charges }}</span>
-    </div>
-      
-    <div class="hp" v-if="hp_needed">
-      <span>&hearts;{{ card.hp }}</span>
-    </div>  
-  
-    
+    <card-comp
+      :card="card"
+      :hp_needed="hp_needed"
+    />
+
   </div>
 </div>
-</div>
-
-<card-modal 
-v-if="hand[i]" 
-:card='hand[i]' 
-:show_card_modal='show_card_modal'
-/>
 
 </template>
 
 <script>
-import { border, background_color } from '@/logic/border_styles'
+import { border } from '@/logic/border_styles'
 
 export default {
   name: 'hand-comp',
@@ -65,22 +33,12 @@ export default {
       default: false
     },
   },
-  data() {
-    return {
-      i: null,  // данные для передачи наверх
-      show_card_modal: false,
-    }
-  },
   methods: {
-    chose_player_card(id) {
-      this.i = id
-      this.$emit('chose_player_card', this.i)  // передаём this.index по эмиту 
+    chose_player_card(index) {
+      this.$emit('chose_player_card', index)  // передаём this.index по эмиту
     },
     border(card, index) {
       return border(card, index)
-    },
-    background_color(card) {
-      return background_color(card)
     },
   },
   emits: [
@@ -98,6 +56,7 @@ export default {
   clear: both;
   overflow: auto;
   white-space: nowrap;
+  position: relative;
 }
 
 .card {
@@ -111,81 +70,5 @@ export default {
   margin-top: 0.1%;
   position: relative;
 }
-
-.img {
-  width: 99%;
-  height: 99%;
-  top: 50%; 
-  left: 50%;
-  transform: translate(-50%, -50%);
-  position: absolute;
-}
-
-.diamond {
-  position: absolute;
-  top: 1%;
-  right: 6%;
-  height: 4vh;
-  width: 4vh;
-  transform: rotateX(45deg) rotateZ(45deg);
-  /* background-color: purple; */
-  /* border: solid 1px yellow; */
-}
-
-span3 {
-  position: absolute;
-  transform: translate(-50%, -50%);
-  top: 25%;
-  right: 2%;
-  color: black;
-  font-size: 10pt;
-  transform:  rotateZ(-45deg);
-}
-
-.circle {
-  position: absolute;
-  width: 34%;
-  height: 4vh;
-  border-radius: 50%;
-  top: 30%;
-  right: 3%;
-}
-
-span {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 8pt;
-}
-
-.charges {
-  width: 20%;
-  height: 20%;
-  background-color: hotpink;
-  position: absolute;
-  bottom: 2%;
-  right: 2%;
-  border-radius: 20%;
-}
-
-.hp {
-  width: 30%;
-  height: 20%;
-  background-color: green;
-  position: absolute;
-  bottom: 2%;
-  left: 2%;
-  border-radius: 20%;
-}
-
-.typee {
-  width: 10%;
-  height: 10%;
-  left: 1%;
-  top: 1%; 
-  position: absolute;
-}
-
 
 </style>
--- src/components/ModalWindows/CardModal.vue
+++ src/components/ModalWindows/CardModal.vue
@@ -1,66 +1,106 @@
 <template>
-<div class="card_view" v-if="show_card_modal">
-  <div class='card_view__content'>
-    <div class='data'>
-      <p>ОПИСАНИЕ</p>
-      <h2> Урон карты - {{ card.damage }} </h2><br>
-      <h2> Заряды карты - {{ card.charges }} </h2><br>
-      <h2> Тип абилки - {{ card.ability }} </h2><br>
-      <h3 v-if="card.type === 'Special'">sp</h3><br>
-    </div>
+<div class="card_view">
+  <button class="close_button" @click="close_self">Закрыть</button>
+  <br>
+
+  <div class="card_border" :style="border(card)">
+    <img class="img" :src="card.image" v-if="card.image" alt="">
   </div>
+
+  <div class="right_panel">
+
+  </div>
+
+  <p>ОПИСАНИЕ</p>
+<!--      <h2> Урон карты - {{ card.damage }} </h2><br>-->
+<!--      <h2> Заряды карты - {{ card.charges }} </h2><br>-->
+<!--      <h2> Тип абилки - {{ card.ability }} </h2><br>-->
+<!--      <h3 v-if="card.type === 'Special'">sp</h3><br>-->
+
 </div>
 </template>
 
 <script>
+import { background_color } from '@/logic/border_styles'
 export default {
   name: 'card-modal',
   props: {
-    show_card_modal: {
-      required: true,
-      type: Boolean
-    },
     card: {
       required: true,
       type: Object
-    }
+    },
   },
+  methods: {
+    close_self() {
+      this.$emit('close_card_modal')
+    },
+    border(card) {
+      if (card.color === 'Bronze') return {'border': 'solid 1px lightsalmon'}
+      else if (card.color === 'Silver') return {'border': 'solid 3px silver'}
+      else if (card.color === 'Gold') return {'border': 'solid 3px gold'}
+      else return {}
+    },
+    background_color(card) {
+      return background_color(card)
+    },
+  },
+  emits: [
+      'close_card_modal',
+  ],
 }
 </script>
 
 <style scoped>
 
 .card_view {
-  background: rgb(100, 6, 143);
-  width: 400px;
-  height: 300px;
+  background-color: greenyellow;
+  width: 100%;
+  height: 98%;
   border-radius: 12px;
   text-align: center;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  font-size: 8pt;
-
+  z-index: 9999;
 }
 
-.card_view__content {
-  width: 300px;
-  height: 200px;
-  border: solid 1px red;
-  border-radius: 5px;
-  font-size: 8pt;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  
+.close_button {
+  margin-top: 2%;
+  width: 20%;
+  height: 5%;
 }
 
-/* ВОПРОС ВОТ ТУТ */
-.data {
-  position: relative;
-  align-self: center;
+h3 {
+  display: inline;
+  font-size: 14pt;
+}
+
+p {
+  font-size: 12pt;
+}
+
+.card_border {
+  width: 65%;
+  height: 55%;
+  display: inline;
+  float: left;
+  margin-left: 1%;
+}
+
+.img {
+  width: 99%;
+  height: 99%;
+  margin: auto;
+}
+
+.right_panel {
+  width: 30%;
+  height: 55%;
+  display: inline;
+  float: right;
+  margin-bottom: 3%;
+  border: solid 2px red;
 }
 
 </style>
