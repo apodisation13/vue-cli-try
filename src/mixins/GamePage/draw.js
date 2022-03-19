2f5f32cd9078362bbfe8a90435267570a953547e
james <Apodisation13@gmail.com>
2022-03-19 13:35:01 +0300

    specialcaseabilities now mixin, everything related to it now there

--- src/mixins/GamePage/draw.js
+++ src/mixins/GamePage/draw.js
@@ -2,9 +2,13 @@ export default {
   data() {
     return {
       can_draw: false,  // возможность вытянуть карту
+      s: 1,  // тестовая переменная, её можно дёргать отсюда, из компонента, из другого миксина!
     }
   },
   methods: {
+    show() {
+      alert(111)
+    },
 
     // можно ли ДРО - ход игрока, карт меньше чем {карт_в_руке}, дека не пуста
     calc_can_draw() {
@@ -13,13 +17,11 @@ export default {
 
     // тянем одну карту из деки, блокируем карты игрока
     draw_one_card() {
-      if (this.can_draw) {
-        let random = Math.floor(Math.random() * this.deck.length);
-        this.hand.push(this.deck[random])
-        this.deck.splice(random, 1)  // удалить этот 0й элемент
-        this.player_cards_active = false
-        this.can_draw = false
-      }
+      let random = Math.floor(Math.random() * this.deck.length);
+      this.hand.push(this.deck[random])
+      this.deck.splice(random, 1)  // удалить этот 0й элемент
+      this.player_cards_active = false
+      this.can_draw = false
     },
 
   },
