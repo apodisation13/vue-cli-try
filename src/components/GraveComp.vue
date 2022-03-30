4c4f3b695fd10e14510c31a7c1539ad9c9342542
james <Apodisation13@gmail.com>
2022-03-30 19:29:14 +0300

    DeckComp, GraveComp, RedrawModal now uses ModalWindow

--- src/components/GraveComp.vue
+++ src/components/GraveComp.vue
@@ -1,16 +1,15 @@
 <template>
 
-  <button class="grave-btn" @click="flag=true"> 
+  <button class="grave-btn"
+    @click="flag=true"
+  >
     Сброс {{ grave_len }}
   </button>
-  
-  <div class="modal_window" v-if="flag" v-touch:swipe="close_self">
-    <button-close @close_self="close_self" />
 
-    <cards-list
-        :cards='grave'
-    />
-  </div>
+  <modal-window v-if="flag" v-touch:swipe="close_self">
+    <button-close @close_self="close_self" />
+    <cards-list :cards='grave' />
+  </modal-window>
 
 </template>
 
@@ -47,19 +46,5 @@
   margin-bottom: 1%;
   margin-top: 1%;
   position: relative;
-}
-
-.modal_window {
-  background-color: limegreen;
-  width: 100%;
-  height: 90%;
-  border-radius: 12px;
-  text-align: center;
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 9999;
-
 }
 </style>
