ade1ed7b174883b1c63fab65509cad9bae6a2f6b
james <Apodisation13@gmail.com>
2022-04-01 12:48:37 +0300

    LevelPage.vue

---
+++ src/components/SelectedDeck.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="selected_deck" v-if="$store.state.current_deck.length > 0">
+    ВЫБРАННАЯ ДЕКА - Жизни {{ $store.state.health }} <br>
+    Лидер {{ $store.state.leader.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "selected-deck",
+}
+</script>
+
+<style scoped>
+.selected_deck {
+  width: 95%;
+  height: 6vh;
+  border: solid 1px black;
+  margin: 1%;
+}
+</style>
