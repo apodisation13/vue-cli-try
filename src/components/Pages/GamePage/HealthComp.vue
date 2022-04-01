4d6d7a71156d879022eb9accd10c081652a2aefc
james <Apodisation13@gmail.com>
2022-04-01 14:00:11 +0300

    HealthComp.vue

---
+++ src/components/Pages/GamePage/HealthComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="health-btn">
+    &hearts;{{ health }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'health-comp',
+  computed: {
+    health() {
+      return this.$store.state.health
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+.health-btn {
+  height: 4vh;
+  width: 98%;
+  background-color: green;
+  border: 0;
+  margin-bottom: 1%;
+  margin-top: 1%;
+  position: relative;
+}
+
+</style>
