3f79a66a7142758d72beba331f297fe199ce5b33
james <Apodisation13@gmail.com>
2022-04-11 17:07:03 +0300

    added toast when leader passively receives charges

--- src/logic/player_move/passive_abilities/add-charges-to-leader-if-play-special.js
+++ src/logic/player_move/passive_abilities/add-charges-to-leader-if-play-special.js
@@ -1,7 +1,12 @@
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
+
+
 function add_charges_to_leader_if_play_special(card, leader) {
   if (card.type === "Special") {
     leader.charges += 1
-    console.log('пассивка лиlера')
+    toast.info('Лидеру добавили 1 заряд')
   }
 }
 
