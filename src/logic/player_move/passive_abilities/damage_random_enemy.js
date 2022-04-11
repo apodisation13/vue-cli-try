5b2fecb35ca1fba62664bcf5575e443a5a7ef7bf
james <Apodisation13@gmail.com>
2022-04-11 17:04:33 +0300

    added passive toast when player card damages

--- src/logic/player_move/passive_abilities/damage_random_enemy.js
+++ src/logic/player_move/passive_abilities/damage_random_enemy.js
@@ -1,5 +1,8 @@
 import {get_random_enemy, hit_one_enemy} from "@/logic/player_move/service/service_for_player_move"
 import {sound_damage_one} from "@/logic/play_sounds"
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
 
 
 function passive_end_turn_damage_random_enemy(field, card, leader, enemy_leader) {
@@ -9,6 +12,7 @@ function passive_end_turn_damage_random_enemy(field, card, leader, enemy_leader)
 
   hit_one_enemy(target, {"damage": 1})
   sound_damage_one()
+  toast.info(`Пассивный урон врагу ${target.name} на 1`)
 
   if (target.hp <= 0) {
     let index = field.indexOf(target)
