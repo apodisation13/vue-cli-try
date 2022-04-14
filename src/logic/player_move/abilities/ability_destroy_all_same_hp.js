d321f23b9aadcf838a50a937eb9d2eaedaf61f75
james <apodisation13@gmail.com>
2022-04-14 11:30:52 +0300

    ability destroy all same hp + optimization for other destroy abilities

--- src/logic/player_move/abilities/ability_destroy_all_same_hp.js
+++ src/logic/player_move/abilities/ability_destroy_all_same_hp.js
@@ -8,8 +8,10 @@ const toast = useToast()
 function destroy_all_same_hp(enemy, field, enemy_leader) {
   let all_enemies = get_all_enemies(field, enemy_leader)
   let targets = all_enemies.filter(e => e.hp === enemy.hp)
+
   toast.warning(`Уничтожили всех врагов, у котороых ${enemy.hp} жизней!`)
   sound_destroy_enemy()
+
   targets.forEach(e => {
     e.hp = 0
   })
