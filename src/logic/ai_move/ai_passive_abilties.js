773115140c0b53bef2ba1573292c2e23ba24bfb0
james <apodisation13@gmail.com>
2022-04-15 19:52:20 +0300

    ai passive ability passive damage

--- src/logic/ai_move/ai_passive_abilties.js
+++ src/logic/ai_move/ai_passive_abilties.js
@@ -1,6 +1,7 @@
 import {passive_end_turn_increase_damage} from "@/logic/ai_move/passive_abilities/increase_damage"
 import {passive_end_turn_heal} from "@/logic/ai_move/passive_abilities/heal"
 import {passive_end_turn_heal_leader} from "@/logic/ai_move/passive_abilities/heal_leader"
+import {passive_end_turn_damage} from "@/logic/ai_move/passive_abilities/damage";
 
 
 function enemy_passive_abilities_end_turn(field, enemy_leader) {
@@ -17,6 +18,10 @@ function enemy_passive_abilities_end_turn(field, enemy_leader) {
 
     else if (enemy.passive_ability.name === 'heal-leader') {
       passive_end_turn_heal_leader(enemy, enemy_leader)
+    }
+
+    else if (enemy.passive_ability.name === 'damage') {
+      passive_end_turn_damage(enemy)
     }
 
   })
