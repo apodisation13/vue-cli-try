f23419d776b5f908fc9e9f36a1c6f5a7c1d27fd6
james <apodisation13@gmail.com>
2022-04-16 21:52:34 +0300

    enemy passives decrease player damage and increase self damage

--- src/logic/ai_move/passive_abilities/decrease_player_damage.js
+++ src/logic/ai_move/passive_abilities/decrease_player_damage.js
@@ -1,13 +1,18 @@
-import {useToast} from 'vue-toastification'
 import {sound_enemy_decrease_player_damage} from "@/logic/play_sounds"
-
-const toast = useToast()
 
 
 function passive_end_turn_decrease_player_damage(enemy, hand) {
   let random = Math.floor(Math.random() * hand.length)
-  hand[random].damage -= 1
-  toast.error(`ВРАГ ${enemy.name} уменьшил урон карты ${hand[random].name} на 1!!!`)
+
+  let temp = hand[random].damage
+
+  hand[random].damage = `${hand[random].damage}-1`
+
+  setTimeout(() => {
+    hand[random].damage = temp
+    hand[random].damage -= 1
+  }, 750)
+
   sound_enemy_decrease_player_damage()
 }
 
--- src/logic/ai_move/passive_abilities/increase_damage.js
+++ src/logic/ai_move/passive_abilities/increase_damage.js
@@ -1,11 +1,18 @@
-import {useToast} from 'vue-toastification'
-
-const toast = useToast()
+import {sound_passive_increase_damage} from "@/logic/play_sounds"
 
 
 function passive_end_turn_increase_damage(enemy) {
-  enemy.damage += enemy.passive_increase_damage
-  toast.info(`Враг ${enemy.name} увеличил свой урон на ${enemy.passive_increase_damage}`)
+  let temp = enemy.damage
+
+  enemy.damage = `${enemy.damage}+${enemy.passive_increase_damage}`
+
+  setTimeout(() => {
+    enemy.damage = temp
+    enemy.damage += enemy.passive_increase_damage
+  }, 750)
+
+  sound_passive_increase_damage()
+
 }
 
 
