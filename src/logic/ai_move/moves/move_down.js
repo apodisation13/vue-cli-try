7f7d6778357f938646c58b8d0a682c9f03dfb6b0
james <apodisation13@gmail.com>
2022-04-16 11:54:40 +0300

    added sound to move down

--- src/logic/ai_move/moves/move_down.js
+++ src/logic/ai_move/moves/move_down.js
@@ -1,16 +1,12 @@
 import store from "@/store"
-import { useToast } from 'vue-toastification'
 import {check_lose} from "@/logic/ai_move/service/check_lose"
-import {sound_enemy_damage_player} from "@/logic/play_sounds"
-
-const toast = useToast()
+import {sound_enemy_damage_player, sound_enemy_move_down} from "@/logic/play_sounds"
 
 
 function down_move(field, i) {
   // враги которые уже стоят внизу
   if (i >= 9) {
     store.commit('change_health', -field[i].damage)
-    toast.error(`враг внизу нанёс урон ${field[i].damage}`)
     sound_enemy_damage_player()
   }
   // ДЛЯ ОСТАЛЬНЫХ ВРАГОВ
@@ -18,14 +14,13 @@ function down_move(field, i) {
     // ЕСЛИ У ВРАГА ЕСТЬ ВРАГ ПОД НИМ ВНИЗУ
     if (field[i+3]) {
       store.commit('change_health', -field[i].damage)
-      toast.error(`враг нанёс урон ${field[i].damage}, потому что ему некуда ходить`)
       sound_enemy_damage_player()
     }
     // ДЛЯ ОСТАЛЬНЫХ, КОМУ ЕСТЬ КУДА ПОХОДИТЬzz
     else {
       field[i+3] = field[i]  // типа враг прыгнул на клеточку ниже
       field[i] = ''
-      // toast.info('враг походил')
+      sound_enemy_move_down()
     }
   }
 
