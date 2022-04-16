df91785029db1723dc9108a0259c8c4fd7cdc2f7
james <apodisation13@gmail.com>
2022-04-16 11:33:04 +0300

    removed toast

--- src/logic/player_move/abilities/ability_heal.js
+++ src/logic/player_move/abilities/ability_heal.js
@@ -1,13 +1,9 @@
 import store from "@/store"
-import { useToast } from 'vue-toastification'
 import {sound_heal} from "@/logic/play_sounds"
-
-const toast = useToast()
 
 
 function heal(card) {
   store.commit('change_health', card.heal)
-  toast.success(`лечение на ${card.heal}`)
   sound_heal()
 }
 
