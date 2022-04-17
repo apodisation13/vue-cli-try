5d0aebca71c5002160fdccb2d73e2091c424ccba
james <apodisation13@gmail.com>
2022-04-17 15:32:32 +0300

    fixes for epa end turn (store)

--- src/logic/ai_move/service/check_lose.js
+++ src/logic/ai_move/service/check_lose.js
@@ -5,7 +5,6 @@ import {sound_lose_game} from "@/logic/play_sounds"
 // функция проверки жизней, проигрыша
 function check_lose(health) {
   if (health <= 0) {
-    alert('Вы проиграли!:(')
     router.push('/')
     sound_lose_game()
   }
