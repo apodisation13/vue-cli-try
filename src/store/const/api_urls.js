297d59312db9b9275f96ce9300d0509b46688168
james <apodisation13@gmail.com>
2022-06-18 12:19:30 +0300

    try 5 - fix URLs

--- src/store/const/api_urls.js
+++ src/store/const/api_urls.js
@@ -1,25 +1,25 @@
 // для login.js
-export const check_auth_url = 'http://194.67.109.190:82/accounts/api-token-auth/'
-export const register_url = 'http://194.67.109.190:82/accounts/register/'
+export const check_auth_url = 'https://card-game.ru/accounts/api-token-auth/'
+export const register_url = 'https://card-game.ru/accounts/register/'
 
 // user_database for database.js
-export const user_database = 'http://194.67.109.190:82/api/v1/user_database/'
+export const user_database = 'https://card-game.ru/api/v1/user_database/'
 
 // post deck, + carddecks + userdeck, user_actions: post_deck
-export const post_deck = 'http://194.67.109.190:82/api/v1/decks/'
+export const post_deck = 'https://card-game.ru/api/v1/decks/'
 
 // patch user_resource
-export const user_resource = 'http://194.67.109.190:82/api/v1/user_resource/'
+export const user_resource = 'https://card-game.ru/api/v1/user_resource/'
 
 // craft card, means POST user:id, card:id
-export const craft_card = 'http://194.67.109.190:82/api/v1/patchcards/craft_user_cards/'
-export const mill_card = 'http://194.67.109.190:82/api/v1/patchcards/mill_user_cards/'
-export const craft_leader = 'http://194.67.109.190:82/api/v1/patchleaders/craft_user_leaders/'
-export const mill_leader = 'http://194.67.109.190:82/api/v1/patchleaders/mill_user_leaders/'
+export const craft_card = 'https://card-game.ru/api/v1/patchcards/craft_user_cards/'
+export const mill_card = 'https://card-game.ru/api/v1/patchcards/mill_user_cards/'
+export const craft_leader = 'https://card-game.ru/api/v1/patchleaders/craft_user_leaders/'
+export const mill_leader = 'https://card-game.ru/api/v1/patchleaders/mill_user_leaders/'
 
 // для рандомного сбора уровня для страницы levels
-export const all_enemies = 'http://194.67.109.190:82/api/v1/enemies/'
-export const all_enemy_leaders = 'http://194.67.109.190:82/api/v1/enemy_leaders/'
+export const all_enemies = 'https://card-game.ru/api/v1/enemies/'
+export const all_enemy_leaders = 'https://card-game.ru/api/v1/enemy_leaders/'
 
 
 // А ВОТ ЭТО ДЛЯ ЛОКАЛЬНОЙ РАЗРАБОТКИ
