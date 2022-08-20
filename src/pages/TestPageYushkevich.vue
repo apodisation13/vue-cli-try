0268ca32f2c7a64c7999c664f3e606acfcd8629b
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-08-20 23:24:01 +0300

    yushkevich create test page

---
+++ src/pages/TestPageYushkevich.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page__wrapper">
+    <h1>Тестовая страница</h1>
+    <div class="page__content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+
+    },
+    methods: {
+
+    },
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
