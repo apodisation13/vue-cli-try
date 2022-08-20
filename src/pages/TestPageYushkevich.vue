0fcd25f7d7ea869e12dcce6aa9d93224b0afd698
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-08-21 00:15:33 +0300

    yushkevich get data of cards from store by getters

--- src/pages/TestPageYushkevich.vue
+++ src/pages/TestPageYushkevich.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="page__wrapper">
     <h1>Тестовая страница</h1>
-    <div class="page__content">
-
+    <div class="page__content cards__list">
+      <div v-for="card in allGameCards" :key="card.id" class="card-block">
+        <img :src="card.card.image" :alt="name" class="imagecard">
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    data: () => {
+    data: () => ({
 
+    }),
+    computed: {
+      allGameCards() {
+        // return [...this.$store.state.database.cards]
+        return this.$store.getters.all_cards; // закомментированная строка выше просто как тест
+      }
     },
-    methods: {
 
-    }
+    created() {
+      (() => {document.title = 'Yushkevich-test'})();
+    },
   }
 </script>
 
 <style scoped>
+.cards__list {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+}
 
+.card-block {
+  border-radius: 10px;
+  overflow: hidden;
+  margin: 3px;
+}
+.imagecard {
+  display: block;
+  max-width: 100px;
+}
 </style>
