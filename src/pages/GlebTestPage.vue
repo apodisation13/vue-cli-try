0500896e0de00666e0b2ace40d1f0f540a9ca2ff
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-08-21 03:42:16 +0300

    test project

---
+++ src/pages/GlebTestPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-for="el in showState" :key="el.id">{{ el.level.name }}</div>
+</template>
+
+<script>
+
+
+export default {
+  name: "GlebTestPage",
+
+  computed: {
+    showState() {
+        // проверка получение данных по модулям из общего стейта
+        // database state factions
+        console.log(this.$store.state.database.factions);
+        // database state обращение как свойствам (геттеры)
+        // console.log(this.$store.database);
+        // вопрос с обращением, откуда из какого хранилища вызываю геттеры, хранилищ может быть много, нет?
+        console.log(this.$store.getters["all_levels"]);
+        // тот же вопрос и с коммитом. Как понял он выполняет роль methods 
+        //  this.$store.commit('set_enemy_leader', this.levels[index].level.enemy_leader)
+      return this.$store.getters["all_levels"];
+    },
+  },
+};
+</script>
+
+<style>
+</style>
