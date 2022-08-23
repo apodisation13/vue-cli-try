38f888bbe6fb4115f2d5b81b64d2cb86148b0d5e
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-08-23 22:54:02 +0300

    change loop for work with obj in ResourceComp

--- src/components/ResourceComp.vue
+++ src/components/ResourceComp.vue
@@ -1,44 +1,36 @@
 <template>
   <div class="resource-list">
-    <div class="resource-item" v-for="item in resources" :key="item.id">
-      <img :src="require(`@/assets/icons/resources/${item.image}`)" :alt="item.ru_name" class="resource-image"/>
-      <div class="resource-count">{{ resource[item.name] }}</div>
+    <div class="resource-item" v-for="(count, name) in resource" :key="count">
+      <img :src="require(`@/assets/icons/resources/${name}.png`)" :alt="name" class="resource-image"/>
+      <div class="resource-count">{{ count}}</div>
+      <!-- <div class="resource-item"> // не удаляю на случай если придется хардкодить
+        <img :src="require('@/assets/'+ 'ico.png')" alt="" class="resource-image"/> <br/>
+        {{ resource.scraps }}
+      </div>
+      <div class="resource-item">
+        <img :src="require('@/assets/'+ 'ico.png')" alt="" class="resource-image"/><br/>
+        {{ resource.wood }}
+      </div>
+      <div class="resource-item">
+        <img :src="require('@/assets/'+ 'ico.png')" alt="" class="resource-image"/><br/>
+        {{ resource.kegs }}
+      </div>
+      <div class="resource-item">
+        <img :src="require('@/assets/'+ 'ico.png')" alt="" class="resource-image"/><br/>
+        {{ resource.big_kegs }}
+      </div>
+      <div class="resource-item">
+        <img :src="require('@/assets/'+ 'ico.png')" alt="" class="resource-image"/><br/>
+        {{ resource.chests }}
+      </div> -->
     </div>
-    <!-- <div class="resources">
-      <img :src="require('@/assets/'+ 'ico.png')" alt="" class="image"/> <br/>
-      {{ resource.scraps }}
-    </div>
-    <div class="resources">
-      <img :src="require('@/assets/'+ 'ico.png')" alt="" class="image"/><br/>
-      {{ resource.wood }}
-    </div>
-    <div class="resources">
-      <img :src="require('@/assets/'+ 'ico.png')" alt="" class="image"/><br/>
-      {{ resource.kegs }}
-    </div>
-    <div class="resources">
-      <img :src="require('@/assets/'+ 'ico.png')" alt="" class="image"/><br/>
-      {{ resource.big_kegs }}
-    </div>
-    <div class="resources">
-      <img :src="require('@/assets/'+ 'ico.png')" alt="" class="image"/><br/>
-      {{ resource.chests }}
-    </div> -->
+
   </div>
 </template>
 
 <script>
 export default {
   name: "resource-comp",
-  data: () => ({
-    resources: [
-      { id: 0, name: 'scraps', ru_name: 'лоскуты', image: 'scrap.png', count: null },
-      { id: 1, name: 'wood', ru_name: 'дерево', image: 'wood.png', count: null },
-      { id: 2, name: 'kegs', ru_name: 'бочки', image: 'keg.png', count: null },
-      { id: 3, name: 'big_kegs', ru_name: 'большие бочки', image: 'big_keg.png', count: null },
-      { id: 4, name: 'chests', ru_name: 'сундуки', image: 'chest.png', count: null }
-    ]
-  }),
   computed: {
     resource() {
       return this.$store.getters["resource"]
@@ -54,10 +46,6 @@ div {
 }
 
 .resource-list {
-  /* float: right; */
-  /*border: solid 1px green;*/
-  /* width: 73%;
-  height: 100%; */
   display: flex;
   align-items: flex-start;
   justify-content: space-around;
@@ -68,14 +56,12 @@ div {
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* display: inline-block; */
-  /*border: solid 1px black;*/
 }
 
 .resource-image {
   display: block;
-  width: 25px;
-  height: 25px;
+  width: 35px;
+  height: 35px;
   margin-bottom: 5px;
 }
 </style>
