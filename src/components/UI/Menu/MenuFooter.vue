fb021e988333d376ef2efb8385df6245e0ffb787
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-08-26 20:56:34 +0300

    изменение стилей футера с фиксед на абсолют, изменение позиционирования кнопок футер

--- src/components/UI/Menu/MenuFooter.vue
+++ src/components/UI/Menu/MenuFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="footer" v-if="menuNeeded">
-    <button class="levels" @click="$router.push('/levelselect')">
+    <button class=" btn-footer levels" @click="$router.push('/levelselect')">
       Уровни
     </button>
-    <button class="game" @click="$router.push('/game')">
+    <button class="btn-footer game" @click="$router.push('/game')">
       Игра
     </button>
-    <button class="deckbuilder" @click="$router.push('/deckbuild')">
+    <button class="btn-footer deckbuilder" @click="$router.push('/deckbuild')">
       Колоды
     </button>
   </div>
@@ -26,36 +26,21 @@ export default {
 
 <style scoped>
 .footer {
-  position: fixed;
-  bottom: 1%;
-  left: 1%;
-  /*border: solid 1px red;*/
-  width: 98%;
-  height: 5vh;
+  position: absolute;
+  width: 100%;
+  padding: 0 10px 10px;
   display: flex;
   text-align: center;
-  margin: auto;
+  justify-content: space-around;
+  bottom: 0;
+}
+
+.btn-footer {
+  padding: 5px 15px;
 }
 
 .game {
-  margin: auto;
-  width: 25vw;
-  height: 100%;
   border-radius: 50%;
-}
-
-.levels {
-  position: absolute;
-  left: 5%;
-  width: 25vw;
-  height: 100%;
-}
-
-.deckbuilder {
-  position: absolute;
-  right: 5%;
-  width: 25vw;
-  height: 100%;
 }
 
 </style>
