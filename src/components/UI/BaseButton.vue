77bfa696de429398897b7510cdcce1eaae3f75ee
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-09-04 18:02:55 +0300

    create base button component

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="base-btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+.btn {
+  display: block;
+}
+</style>
--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -6,7 +6,9 @@
 
 <script>
 export default {
-
+created() {
+  console.log('мы на страгице настройки')
+}
 }
 </script>
 
