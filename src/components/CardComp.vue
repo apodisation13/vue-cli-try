8dc90ed7158d66ef9d40b887a4daf0c14e1ae752
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-09-11 15:00:38 +0300

    reset to back card

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,22 +1,22 @@
 <template>
   <div
-    class="card-item"
-    :style="{ backgroundImage: `url(${this.card.image})` }"
-    :id="make_id(card, index)"
+    @contextmenu.prevent
+    @click.right="show_modal"
+    v-touch:longtap="show_modal"
   >
-    <!-- @contextmenu.prevent
-    @click.right="show_modal" эти события понадобятся позже
-    v-touch:longtap="show_modal" -->
-    <!-- <img
+    <img
       :class="!deckbuilder || count > 0 ? 'img' : 'img2'"
       :src="card.image"
       alt=""
       v-if="card.image"
       :id="make_id(card, index)"
-    /> -->
+    />
 
-    <!-- <card-type v-if="card.type === 'Special'">&starf;</card-type> -->
-    <card-damage-icon :style="background_color(card)" :damage="card.damage" />
+    <card-type v-if="card.type === 'Special'">&starf;</card-type>
+
+    <card-diamond :style="background_color(card)"
+      >&dagger;{{ card.damage }}</card-diamond
+    >
 
     <card-ability-circle :card="card" />
 
@@ -29,25 +29,25 @@
     <card-charges>{{ card.charges }}&#8607;</card-charges>
 
     <card-hp v-if="hp_needed">&hearts;{{ card.hp }}</card-hp>
-    <!-- 
+
     <card-modal
       v-if="show_card_modal"
       :card="card"
       :hp_needed="hp_needed"
       @close_card_modal="show_card_modal = false"
-    /> -->
+    />
   </div>
 </template>
 
 <script>
 import { background_color } from "@/logic/border_styles"
 import CardModal from "@/components/ModalWindows/CardModal"
+import CardDiamond from "@/components/UI/CardDiamond"
 import CardType from "@/components/UI/CardType"
 import CardAbilityCircle from "@/components/UI/AbilityCircleCard"
 import CardPassive from "@/components/UI/CardPassive"
 import CardCharges from "@/components/UI/CardCharges"
 import CardHp from "@/components/UI/CardHp"
-import CardDamageIcon from "@/components/UI/CardDamageIcon"
 export default {
   name: "card-comp",
   components: {
@@ -56,8 +56,8 @@ export default {
     CardPassive,
     CardAbilityCircle,
     CardType,
+    CardDiamond,
     CardModal,
-    CardDamageIcon,
   },
   props: {
     card: {
@@ -75,9 +75,11 @@ export default {
     },
     count: {
       type: Number,
+      required: false,
     },
     index: {
       type: Number,
+      required: false,
     },
   },
   data() {
@@ -101,13 +103,26 @@ export default {
 </script>
 
 <style scoped>
-.card-item {
-  width: 70px;
-  height: 100px;
-  margin: 8px;
-  position: relative;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
+.img {
+  width: 99.3%;
+  height: 99.3%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  border-width: 3px 4px 3px 5px;
+  border-radius: 95% 4% 92% 5%/4% 95% 6% 95%;
+}
+
+.img2 {
+  width: 99.3%;
+  height: 99.3%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  opacity: 0.25;
+  border-width: 3px 4px 3px 5px;
+  border-radius: 95% 4% 92% 5%/4% 95% 6% 95%;
 }
 </style>
