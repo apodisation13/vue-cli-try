477160257198e4a02593cb1724abcb93a0876bb6
fixer400 <fixer400@yandex.ru>
2022-09-11 13:30:59 +0300

    Merged dev into main page

--- src/components/UI/Menu/MenuHeader.vue
+++ src/components/UI/Menu/MenuHeader.vue
@@ -1,68 +1,50 @@
 <template>
   <div class="header" v-if="menuNeeded">
-<<<<<<< HEAD
-    <div class="header-fixed">
+    <div class="wrapper__avatar-resources">
       <button class="avatar" @click="showExpandedMenu" v-touch:swipe.bottom="showExpandedMenu">
         <img :src="require('@/assets/icons/'+ 'Avatar.svg')" alt="">
-=======
-    <div class="wrapper__avatar-resources">
-      <button
-        class="avatar"
-        @click="showExpandedMenu"
-        v-touch:swipe.bottom="showExpandedMenu"
-      >
-        <img :src="require('@/assets/icons/' + 'Avatar.svg')" alt="" />
->>>>>>> dev
       </button>
       <resource-list @click="$router.push('/bonus')" />
     </div>
-    <div
-      class="expand-menu"
-      v-if="expanded"
-      v-touch:swipe.top="showExpandedMenu"
-    >
-      <div class="expand-menu__content">
-        <div class="expand-menu__action" v-for="button in routes" :key="button">
-          <button class="menu-btn" @click="push(button.path)">
-            {{ button.title }}
-          </button>
-          <span>$</span>
-        </div>
-        <div class="expand-menu__action bordered" @click="showExpandedMenu">
-          <button class="menu-btn">Закрыть</button>
-          <span>X</span>
+    
+      <div class="expand-menu" v-if="expanded" v-touch:swipe.top="showExpandedMenu" >
+        <div class="expand-menu__content" >
+
+          <div class="expand-menu__action" v-for="button in routes" :key="button">
+            <button class="menu-btn"
+            @click="push(button.path)"
+            >
+              {{ button.title }}
+            </button>
+            <span>$</span>
+          </div>
+          <div class="expand-menu__action bordered" @click="showExpandedMenu">
+            <button class="menu-btn">
+              Закрыть
+            </button>
+            <span>X</span>
+          </div>
         </div>
       </div>
-<<<<<<< HEAD
 
-=======
->>>>>>> dev
-    </div>
   </div>
 </template>
 
 <script>
-//import ResourceComp from "@/components/ResourceComp"
+import ResourceList from "@/components/ResourceList"
 export default {
   name: "MenuHeader",
-<<<<<<< HEAD
-  components: {ResourceComp},
-=======
-  components: { ResourceList },
->>>>>>> dev
+  components: {ResourceList},
   computed: {
     // меню не нужны, если в роутере есть notRequireMenu (страницы загрузки, игры)
     menuNeeded() {
-      return (
-        !this.$store.state.isGame &&
-        !this.$router.currentRoute.value.meta.notRequireMenu
-      )
+      return (!this.$store.state.isGame && !this.$router.currentRoute.value.meta.notRequireMenu)
     },
     username() {
-      return this.$store.getters["getUser"].username
+      return this.$store.getters['getUser'].username
     },
     isLoggedIn() {
-      return this.$store.getters["isLoggedIn"]
+      return this.$store.getters['isLoggedIn']
     },
   },
   data() {
@@ -70,8 +52,8 @@ export default {
       routes: [
         { title: "Главная", path: "/" },
         { title: "Правила", path: "/rules" },
-        { title: "О нас", path: "/about" },
         { title: "Настройки", path: "/settings" },
+        { title: "О нас", path: "/about" },
       ],
       expanded: false,
     }
@@ -94,7 +76,7 @@ export default {
 
 <style scoped>
 @media screen and (max-width: 1000px) {
-  .header-fixed {
+  .header {
     /*border: solid 1px yellow;*/
     width: 99%;
     height: 7vh;
@@ -102,14 +84,14 @@ export default {
 }
 
 @media screen and (max-width: 500px) {
-  .header-fixed {
+  .header {
     /*border: solid 1px red;*/
     width: 99%;
     height: 7vh;
   }
 }
 
-.avatar {
+.avatar{
   background: none;
   border: none;
 }
@@ -122,12 +104,6 @@ export default {
 }
 
 .expand-menu__content {
-<<<<<<< HEAD
-=======
-  position: fixed;
-  left: 15px;
-  top: 15px;
->>>>>>> dev
   display: flex;
   margin-top: 18px;
   margin-left: 10px;
@@ -135,23 +111,16 @@ export default {
   justify-content: space-around;
   width: 156px;
   height: 198px;
-  background: #ffffff;
+  background: #FFFFFF;
   border-radius: 8px;
 }
 
 .expand-menu {
   content: "";
-<<<<<<< HEAD
   position: absolute;
   background-color: rgba(0, 0, 0, 0.3);
   left: 0px;
   top: 0px;
-=======
-  background-color: rgba(0, 0, 0, 0.3);
-  position: fixed;
-  left: 0;
-  top: 0;
->>>>>>> dev
   width: 100%;
   height: 100%;
   z-index: 2;
@@ -160,22 +129,23 @@ export default {
 .menu-btn {
   margin: 1px;
   border: none;
-  background-color: #ffffff;
+  background-color:#FFFFFF;
 }
 
-.menu-btn:active {
+.menu-btn:active{
   color: #263238;
 }
 
-.expand-menu__action {
+.expand-menu__action{
   display: flex;
   justify-content: space-between;
   padding-left: 16px;
   padding-right: 11px;
 }
 
-.bordered {
+.bordered{
   border-top: 1px solid rgba(38, 50, 56, 0.16);
   padding-top: 11px;
 }
+
 </style>
