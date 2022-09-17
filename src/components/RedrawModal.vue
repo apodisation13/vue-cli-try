f6b82a5e2f3d11eff619d26359da06e0e83cd98e
james <apodisation13@gmail.com>
2022-09-17 17:21:07 +0300

    redraw

--- src/components/RedrawModal.vue
+++ src/components/RedrawModal.vue
@@ -3,17 +3,14 @@
     <button-close @close_self="close_self" />
 
     <div>
-      <cards-list
-        :cards="hand_current"
-        @chose_player_card="chose_player_card"
-      />
+      <cards-list :cards="hand" @chose_player_card="chose_player_card" />
     </div>
 
     <div>
       <h1>Изменить карту можно ещё {{ redraws }} раз</h1>
     </div>
-    <h2>В колоде осталось ещё {{ deck_current.length }} карт</h2>
-    <cards-list :cards="deck_current" />
+    <h2>В колоде осталось ещё {{ deck.length }} карт</h2>
+    <cards-list :cards="deck" />
   </modal-window>
 </template>
 
@@ -21,6 +18,7 @@
 import ModalWindow from "@/components/UI/ModalWindow"
 import CardsList from "@/components/CardsList"
 import ButtonClose from "@/components/UI/ButtonClose"
+import { choice } from "@/lib/utils"
 export default {
   name: "redraw-modal",
   components: { ButtonClose, CardsList, ModalWindow },
@@ -28,51 +26,42 @@ export default {
     return {
       visible: true,
       redraws: 3,
-      hand_current: this.hand.slice(),
-      deck_current: this.deck.slice(),
+      hand: this.gameObj.hand.slice(),
+      deck: this.gameObj.deck.slice(),
       redraw_array: [],
     }
   },
   props: {
-    deck: {
+    gameObj: {
       required: true,
-      type: Array,
-    },
-    hand: {
-      required: true,
-      type: Array,
+      type: Object,
     },
   },
   created() {
-    if (!this.deck.length) this.close_self()
+    if (!this.gameObj.deck.length) this.close_self()
   },
   methods: {
     close_self() {
       this.visible = false
 
       this.redraw_array.forEach(element => {
-        this.deck_current.push(element) // вернуть в деку карты
+        this.deck.push(element) // вернуть в деку карты
       })
 
       this.$emit("redraw_finished", {
-        deck: this.deck_current,
-        hand: this.hand_current,
+        deck: this.deck,
+        hand: this.hand,
       })
     },
 
     chose_player_card(card) {
       this.redraws -= 1
-
       this.redraw_array.push(card) // добавили карту в темп-список
-      let random = Math.floor(Math.random() * this.deck_current.length)
-      this.hand_current.splice(
-        this.hand_current.indexOf(card),
-        1,
-        this.deck_current[random]
-      ) // обновить
-      this.deck_current.splice(random, 1) // удалить этот i-й элемент
-
-      if (this.redraws === 0 || !this.deck_current.length) this.close_self()
+      let random = choice(this.deck)
+      // заменяем ту карту, на которую ткнули, на новую случайную карту из колоды
+      this.hand.splice(this.hand.indexOf(card), 1, this.deck[random])
+      this.deck.splice(random, 1) // удалить этот i-й элемент
+      if (this.redraws === 0 || !this.deck.length) this.close_self()
     },
   },
 }
