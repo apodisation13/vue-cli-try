338000f7d0c657206e162d2f42373016735cd402
fixer400 <fixer400@yandex.ru>
2022-09-25 16:51:58 +0300

    UI для главного меню
    -Добавил стили для новостей и шапки
    -Добавил дату публикации на компонент с новостью в "ромбе"
    -Осталась проблема с цветом шрифта в всплывающем меню при клике на аватарку, нужно будет подумать как исправить ее

--- src/components/UI/ResourceItem.vue
+++ src/components/UI/ResourceItem.vue
@@ -35,23 +35,38 @@ export default {
 }
 
 .wrapper__resource-image {
-  width: 35px;
-  height: 35px;
-  margin-bottom: 5px;
   display: flex;
   align-items: flex-start;
   justify-content: center;
 }
+
 .resource-image {
   display: block;
-  /* max-width: 35px; */
-  max-height: 35px;
+  width: 35px;
+  max-height: 30px;
+}
+
+.resource-count {
+  font-family: "Philosopher";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 120%;
+  background: linear-gradient(
+    183.6deg,
+    #edb13e 2.96%,
+    #f4d977 65.79%,
+    #eeb850 129.95%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
 }
 
 @media (max-width: 640px) {
   .wrapper__resource-image {
     /* width: 25px; */
-    height: 25px;
+    max-height: 25px;
   }
   .resource-image {
     /* max-width: 25px; */
