61eac1fc0b5cf64bebec10ac0bc58453b3afd7e5
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-10-31 22:43:44 +0300

    add cardnodal to assembling build

--- src/components/CardItemPreview.vue
+++ src/components/CardItemPreview.vue
@@ -1,14 +1,27 @@
 <template>
-  <div
-    v-if="!!user_card"
-    class="card-item"
-    :style="[{ backgroundImage: `url(${user_card.image})` }, border(user_card)]"
-  ></div>
+  <div class="card-item-wrapper"
+    @contextmenu.prevent
+    @click.right="show_modal"
+    v-touch:longtap="show_modal"
+  >
+    <div
+      class="card-item"
+      v-if="!!user_card"
+      :style="[{ backgroundImage: `url(${user_card.image})` }, border(user_card)]"
+    ></div>
+    <card-modal
+      v-if="show_card_modal"
+      :card="user_card"
+      @close_card_modal="show_card_modal = false"
+    />
+  </div>
 </template>
 
 <script>
 import { border_leader, border_for_card } from "@/logic/border_styles"
+import CardModal from '@/components/ModalWindows/CardModal'
 export default {
+  components: { CardModal },
   props: {
     user_card: {
       type: Object,
@@ -19,19 +32,29 @@ export default {
       default: false,
     },
   },
+  data() {
+    return {
+      show_card_modal: false,
+    }
+  },
   methods: {
     border(card) {
       return this.is_leader ? border_leader(card) : border_for_card(card)
     },
+    show_modal() {
+      this.show_card_modal = true
+    }
   },
 }
 </script>
 
 <style scoped>
+.card-item-wrapper {
+  width: 100%;
+}
 .card-item {
   position: relative;
   width: 100%;
-  position: relative;
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
