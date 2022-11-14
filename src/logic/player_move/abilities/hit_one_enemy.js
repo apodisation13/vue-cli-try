eec3607bcf93c60d9ac110c77e427422266875d8
james <apodisation13@gmail.com>
2022-11-14 20:24:38 +0300

    gameObj

--- src/logic/player_move/abilities/hit_one_enemy.js
+++ src/logic/player_move/abilities/hit_one_enemy.js
@@ -1,15 +1,7 @@
 import { sound_hit_shield } from "@/logic/play_sounds"
-import { check_win } from "@/logic/player_move/service/check_win"
-import { set_dead_and_deathwish } from "@/logic/player_move/service/service_for_player_move"
+import { enemy_takes_damage } from "@/logic/player_move/abilities/enemy_takes_damage"
 
-function hit_one_enemy(
-  enemy,
-  card,
-  field,
-  enemy_leader,
-  enemies,
-  timeout = 1000
-) {
+function hit_one_enemy(enemy, card, gameObj, timeout = 1000) {
   card.damages_enemy = true
 
   if (enemy.shield) {
@@ -19,31 +11,7 @@ function hit_one_enemy(
     return
   }
 
-  // поставим врагу enemy.deathwish_triggered
-  set_dead_and_deathwish(enemy, card)
-
-  let temp = enemy.hp // сложили жизни в темп
-  enemy.hp = `${enemy.hp}-${card.damage}` // отрисовали 5-3 (жизни-урон)
-
-  // через timeout, default=1сек
-  setTimeout(() => {
-    enemy.hp = temp // вернули ему жизни
-    enemy.hp -= card.damage // вот теперь отняли урон
-
-    if (enemy.hp <= 0) {
-      // это поле признак именно лидера врагов
-      if (enemy?.damage_once >= 0) {
-        enemy_leader.hp = 0
-        console.log("умер лидер врагов")
-      } else {
-        field[field.indexOf(enemy)] = ""
-        console.log("враг умер")
-      }
-    }
-
-    card.damages_enemy = false
-    check_win(field, enemies, enemy_leader)
-  }, timeout)
+  enemy_takes_damage(enemy, card, gameObj, timeout)
 }
 
 export { hit_one_enemy }
