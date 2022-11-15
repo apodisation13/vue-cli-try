17f7bc972b82088f901da55998a0eff52ca254c7
james <apodisation13@gmail.com>
2022-11-15 16:18:01 +0300

    deathwish logic

--- src/logic/ai_move/deathwish_abilities/spawn_tokens.js
+++ src/logic/ai_move/deathwish_abilities/spawn_tokens.js
@@ -1,12 +1,15 @@
 import store from "@/store"
 import { get_empty_field_indexes } from "@/logic/player_move/service/service_for_player_move"
 import { copyObj } from "@/lib/utils"
+import { sound_deathwish } from "@/logic/play_sounds"
 
 export function spawn_tokens(enemy, field) {
   const defaultEnemy = copyObj(
     store.getters["all_enemies"].find(e => e.id === enemy.id)
   )
   if (!defaultEnemy) return
+
+  sound_deathwish()
 
   defaultEnemy.has_deathwish = false
   defaultEnemy.hp = 1
