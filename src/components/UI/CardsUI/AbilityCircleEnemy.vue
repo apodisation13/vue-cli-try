f635420598bb42ece5e846d034e84cf292e03e84
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-11-19 21:16:26 +0300

    AbilityCircleEnemy optimisation

--- src/components/UI/CardsUI/AbilityCircleEnemy.vue
+++ src/components/UI/CardsUI/AbilityCircleEnemy.vue
@@ -1,20 +1,14 @@
 <template>
-  <enemy-circle v-if="enemy.move.name === 'down'" move="enemy_move_down" />
-  <enemy-circle
-    v-else-if="enemy.move.name === 'stand'"
-    move="enemy_move_stand"
-  />
-  <enemy-circle
-    v-else-if="enemy.move.name === 'random'"
-    move="enemy_move_random"
-  />
+    <div class="ability-circle-enemy"
+      v-if="!!enemy.move.name"
+      :style="{
+        'background-image': 'url(' + require(`@/assets/icons/enemy/enemy_move_${enemy.move.name}.svg`) + ')',
+      }"
+  ></div>
 </template>
 
 <script>
-import EnemyCircle from "@/components/UI/CardsUI/EnemyCircle"
 export default {
-  name: "ability-circle-enemy",
-  components: { EnemyCircle },
   props: {
     enemy: {
       type: Object,
@@ -24,4 +18,12 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.ability-circle-enemy {
+  width: 25px;
+  height: 25px;
+  top: 5px;
+  left: 5px;
+  background-repeat: no-repeat;
+}
+</style>
