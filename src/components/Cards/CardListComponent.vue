7c0efe1fd44e6827798a6e53b82ed32beeac8235
james <apodisation13@gmail.com>
2022-11-25 15:49:45 +0300

    some commentaries

--- src/components/Cards/CardListComponent.vue
+++ src/components/Cards/CardListComponent.vue
@@ -1,15 +1,17 @@
 <template>
+  <!--Отображает любой список карт или список лидеров (is_leader)-->
   <div class="card-list">
+    <!--Двойной клик по карте - событие действия на картой-->
     <card-item
       v-for="user_card in cards"
       :key="user_card.id"
+      @dblclick="chose_player_card(user_card)"
       :count="user_card.count"
       :card="user_card.card ? user_card.card : user_card"
       :user_card="user_card.card ? user_card : null"
       :hp_needed="hp_needed"
       :deckbuilder="deckbuilder"
       :bonus="bonus"
-      @dblclick="chose_player_card(user_card)"
       :is_leader="is_leader"
     />
   </div>
@@ -22,24 +24,27 @@ export default {
     CardItem,
   },
   props: {
+    // собственно список карт, которые надо отрисовать
     cards: {
       required: true,
       type: Array,
     },
+    // показывать или не показывать hp (в игре не нужны жизни, везде нужны)
     hp_needed: {
-      // hp только для декбилдера, для игры не нужно оно
       type: Boolean,
       default: false,
     },
+    // показывать или не показывать зону кнопок милл\крафт, только для декбилдера
     deckbuilder: {
       type: Boolean,
       default: false,
     },
+    // брать ли границу карты как для карт (по цвету), ДЕФОЛТНОЕ, или как для лидеров (по фракции)
     is_leader: {
-      // брать границу карты как для лидеров
       type: Boolean,
       default: false,
     },
+    // на странице бонусов мы показываем count, но не показываем mill/craft
     bonus: {
       type: Boolean,
       default: false,
@@ -48,7 +53,7 @@ export default {
   },
   methods: {
     chose_player_card(card) {
-      this.$emit("chose_player_card", card) // передаём this.index по эмиту
+      this.$emit("chose_player_card", card) // передаем наверх ВСЮ выбранную карту!
     },
   },
   emits: ["chose_player_card"],
