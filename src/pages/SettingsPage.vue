a8af5dc247c5429281a9704fcb40de0078d77bdd
james <apodisation13@gmail.com>
2022-12-02 15:07:49 +0300

    better settings

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,34 +1,49 @@
 <template>
   <div class="settings">
-    <h1 class="setting-title">Настройки</h1>
-    <div @click="selectedSetting = null" style="margin-bottom: 10px">Назад</div>
+    <h1 class="text">Настройки</h1>
+    <div
+      @click="goBack"
+      class="back_icon"
+      v-if="selectedSetting !== null"
+    ></div>
+
     <!--Верхний уровень настроек-->
     <div v-if="selectedSetting === null">
-      <div
-        v-for="(setting, index) in settings"
-        :key="index"
-        @click="selectedSetting = index"
-      >
-        <base-button class="base-button">
-          <div class="text">{{ setting }}</div>
-        </base-button>
-      </div>
+      <settings-list :settings="settings" @select-setting="selectSetting" />
     </div>
 
     <!--Уровень настроек "общие настройки"-->
-    <div class="settings-list" v-if="selectedSetting === 0">
-      <setting-sound class="setting-item" />
+    <div v-if="selectedSetting === 0">
+      <settings-list
+        :settings="mainSettings"
+        @select-setting="selectMainSetting"
+      >
+        <setting-sound v-if="selectedMainSetting === 0" />
+        <!--Сюда так же добавим ещё настройки-->
+      </settings-list>
     </div>
 
     <!--Уровень настроек "аккаунт"-->
-    <div class="settings-list" v-if="selectedSetting === 1">
-      <setting-logout class="setting-item" />
-      <setting-delete-all-levels class="setting-item" />
+    <div v-if="selectedSetting === 1">
+      <settings-list
+        :settings="accountSettings"
+        @select-setting="selectAccountSetting"
+      >
+        <!--Сюда так же добавим ещё настройки-->
+        <setting-delete-all-levels v-if="selectedAccountSetting === 2" />
+        <setting-logout v-if="selectedAccountSetting === 3" />
+      </settings-list>
     </div>
 
     <!--Уровень настроек "персонализация"-->
-    <div class="settings-list" v-if="selectedSetting === 2">
-      <setting-choose-theme class="setting-item" />
+    <div v-if="selectedSetting === 2">
+      <settings-list
+        :settings="personalSettings"
+        @select-setting="selectPersonalSetting"
+      >
+        <setting-choose-theme v-if="selectedPersonalSetting === 0" />
+        <!--Сюда так же добавим ещё настройки-->
+      </settings-list>
     </div>
   </div>
 </template>
@@ -37,12 +52,12 @@
 import SettingLogout from "@/components/Pages/SettingsPage/SettingLogout"
 import SettingDeleteAllLevels from "@/components/Pages/SettingsPage/SettingDeleteAllLevels"
 import SettingSound from "@/components/Pages/SettingsPage/SettingSound"
-import BaseButton from "@/components/UI/Buttons/BaseButton.vue"
 import SettingChooseTheme from "@/components/Pages/SettingsPage/SettingChooseTheme.vue"
+import SettingsList from "@/components/Pages/SettingsPage/SettingsList.vue"
 export default {
   components: {
+    SettingsList,
     SettingChooseTheme,
-    BaseButton,
     SettingSound,
     SettingDeleteAllLevels,
     SettingLogout,
@@ -50,8 +65,34 @@ export default {
   data() {
     return {
       settings: ["Общие настройки", "Аккаунт", "Персонализация"],
+      mainSettings: ["Звук", "Язык", "Яркость"],
+      accountSettings: ["Данные", "Пароль", "Сброс уровней", "Выйти"],
+      personalSettings: ["Цветовая тема", "Аватар", "Стиль карт"],
       selectedSetting: null,
+      selectedMainSetting: null,
+      selectedAccountSetting: null,
+      selectedPersonalSetting: null,
     }
+  },
+  methods: {
+    selectSetting(index) {
+      this.selectedSetting = index
+    },
+    selectMainSetting(index) {
+      this.selectedMainSetting = index
+    },
+    selectAccountSetting(index) {
+      this.selectedAccountSetting = index
+    },
+    selectPersonalSetting(index) {
+      this.selectedPersonalSetting = index
+    },
+    goBack() {
+      this.selectedSetting = null
+      this.selectedMainSetting = null
+      this.selectedPersonalSetting = null
+      this.selectedAccountSetting = null
+    },
   },
 }
 </script>
@@ -61,26 +102,26 @@ export default {
   padding: 15px;
 }
 
-.setting-title {
-  text-align: center;
-  margin-bottom: 66px;
-}
-
-.setting-item:nth-child(n + 2) {
-  margin-top: 13px;
-}
-
-.base-button {
-  background: linear-gradient(#1d252d, #000000, #282d33);
-  margin-bottom: 13px;
-  border: 2px solid #facf5d;
+.back_icon {
+  position: absolute;
+  left: 22px;
+  top: 115px;
+  width: 35px;
+  height: 35px;
+  background-image: url("~@/assets/icons/buttons/back_icon.svg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .text {
   font-family: "Philosopher", serif;
   font-style: normal;
   font-weight: 700;
-  font-size: 16px;
+  font-size: 29px;
   line-height: 22px;
   text-align: center;
   letter-spacing: -0.02em;
@@ -96,5 +137,7 @@ export default {
   background-clip: text;
   text-fill-color: transparent;
   text-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
+  margin-bottom: 50px;
+  margin-top: 8px;
 }
 </style>
