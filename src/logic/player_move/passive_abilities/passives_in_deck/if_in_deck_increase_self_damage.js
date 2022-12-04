dfa63a205ddf0b87046304c939fd9f8f14e2fb1f
james <apodisation13@gmail.com>
2022-12-04 15:19:14 +0300

    passives in deck, grave doesn't have toast anymore

--- src/logic/player_move/passive_abilities/passives_in_deck/if_in_deck_increase_self_damage.js
+++ src/logic/player_move/passive_abilities/passives_in_deck/if_in_deck_increase_self_damage.js
@@ -1,10 +1,5 @@
-import { useToast } from "vue-toastification"
-
-const toast = useToast()
-
 function if_in_deck_increase_self_damage(card) {
   card.damage += card.value
-  toast.info(`Карта ${card.name} увеличила урон, находясь в колоде`)
 }
 
 export { if_in_deck_increase_self_damage }
--- src/logic/player_move/passive_abilities/passives_in_grave/if_in_grave_increase_self_damage.js
+++ src/logic/player_move/passive_abilities/passives_in_grave/if_in_grave_increase_self_damage.js
@@ -1,10 +1,5 @@
-import { useToast } from "vue-toastification"
-
-const toast = useToast()
-
 function if_in_grave_increase_self_damage(card) {
   card.damage += card.value
-  toast.info(`Карта ${card.name} увеличила урон, находясь в сбросе`)
 }
 
 export { if_in_grave_increase_self_damage }
