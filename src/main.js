66f4b92ff4d8c20dda2f73ebc22e858a1a888cf3
Yushkevich-A-A <Mortifer1993@yandex.ru>
2022-12-10 14:32:27 +0300

    lint

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue"
 import App from "@/App.vue"
-import './assets/css/main.css';
+import "@/assets/css/main.css"
 import router from "@/router/router"
 import store from "@/store"
 import Toast from "vue-toastification"
