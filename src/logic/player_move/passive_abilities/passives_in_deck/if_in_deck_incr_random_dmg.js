afe280724f80e77fc96e0178d0e9be3129a0bea0
james <apodisation13@gmail.com>
2022-12-20 18:44:28 +0300

    NEW TIMER LOGIC!

--- src/logic/player_move/passive_abilities/passives_in_deck/if_in_deck_incr_random_dmg.js
+++ src/logic/player_move/passive_abilities/passives_in_deck/if_in_deck_incr_random_dmg.js
@@ -1,10 +1,9 @@
 import { choice } from "@/lib/utils"
-import { timeoutAnimationFlag, timer } from "@/logic/game_logic/timers"
+import { timeoutAnimationFlag } from "@/logic/game_logic/timers"
 
 export function if_in_deck_incr_random_dmg(card, gameObj) {
   const { deck } = gameObj
   if (!deck) return
-  if (card.default_timer && (card.timer === 0 || !timer(card))) return
 
   timeoutAnimationFlag(card, "trigger_deck_passive")
   deck[choice(deck)].damage += card.value
