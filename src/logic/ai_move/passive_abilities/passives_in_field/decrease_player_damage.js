836a0eae201eb21ae525a31442361c6244d73cad
james <apodisation13@gmail.com>
2022-12-27 14:43:39 +0300

    passives enemies

--- src/logic/ai_move/passive_abilities/passives_in_field/decrease_player_damage.js
+++ src/logic/ai_move/passive_abilities/passives_in_field/decrease_player_damage.js
@@ -1,18 +1,21 @@
 import { sound_enemy_decrease_player_damage } from "@/logic/play_sounds"
+import { choice } from "@/lib/utils"
+import {
+  timeoutAnimationFlag,
+  timeoutAnimationValue,
+} from "@/logic/game_logic/timers"
 
 function decrease_player_damage(enemy, hand) {
-  let random = Math.floor(Math.random() * hand.length)
-
-  let temp = hand[random].damage
-
-  hand[random].damage = `${hand[random].damage}-${enemy.value}`
-
-  setTimeout(() => {
-    hand[random].damage = temp
-    hand[random].damage -= enemy.value
-  }, 750)
-
-  sound_enemy_decrease_player_damage()
+  let random = choice(hand)
+  timeoutAnimationValue(
+    hand[random],
+    "damage",
+    `${hand[random].damage}-${enemy.value}`,
+    enemy.value * -1,
+    sound_enemy_decrease_player_damage,
+    750
+  )
+  timeoutAnimationFlag(enemy, "damages_player")
 }
 
 export { decrease_player_damage }
--- src/logic/ai_move/passive_abilities/passives_in_field/increase_damage.js
+++ src/logic/ai_move/passive_abilities/passives_in_field/increase_damage.js
@@ -1,16 +1,18 @@
 import { sound_passive_increase_damage } from "@/logic/play_sounds"
+import {
+  timeoutAnimationFlag,
+  timeoutAnimationValue,
+} from "@/logic/game_logic/timers"
 
 function incr_self_dmg(enemy) {
-  let temp = enemy.damage
-
-  enemy.damage = `${enemy.damage}+${enemy.value}`
-
-  setTimeout(() => {
-    enemy.damage = temp
-    enemy.damage += enemy.value
-  }, 750)
-
-  sound_passive_increase_damage()
+  timeoutAnimationFlag(enemy, "incr_dmg")
+  timeoutAnimationValue(
+    enemy,
+    "damage",
+    `${enemy.damage}+${enemy.value}`,
+    enemy.value,
+    sound_passive_increase_damage
+  )
 }
 
 export { incr_self_dmg }
