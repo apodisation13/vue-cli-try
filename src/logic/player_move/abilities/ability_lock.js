c0ffe8fb134f6498531c1b786f1a524b3e7c4fa2
james <apodisation13@gmail.com>
2022-12-31 22:58:47 +0300

    fix lock

--- src/logic/player_move/abilities/ability_lock.js
+++ src/logic/player_move/abilities/ability_lock.js
@@ -1,7 +1,12 @@
 function lock_enemy(enemy) {
   enemy.shield = false
   enemy.has_passive = false
+  enemy.has_passive_in_field = null
+  enemy.has_passive_in_deck = null
+  enemy.has_passive_in_grave = null
+  enemy.passive_ability = null
   enemy.has_deathwish = false
+  enemy.deathwish = null
   enemy.locked = true
 }
 
