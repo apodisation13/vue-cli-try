10aea42ae20b65d0593fdf20a4b3fdd2038401f5
james <apodisation13@gmail.com>
2022-12-31 14:07:05 +0300

    new abilities

--- src/logic/ai_move/passive_abilities/passives_grave.js
+++ src/logic/ai_move/passive_abilities/passives_grave.js
@@ -1,3 +1,18 @@
-export function grave_passives(gameObj) {
-  console.log(gameObj)
+import {
+  allowActionTimer,
+  timeoutAnimationFlag,
+} from "@/logic/game_logic/timers"
+import { heal_random } from "@/logic/ai_move/passive_abilities/passives_in_field/heal"
+
+export function grave_passives(enemy, gameObj) {
+  if (!allowActionTimer(enemy)) return
+
+  const { enemy_leader, field, enemies_grave } = gameObj
+
+  const pea = enemy.passive_ability.name
+
+  if (pea === "if-in-grave-heal-random") {
+    heal_random(enemy, field, enemy_leader)
+    timeoutAnimationFlag(enemies_grave[0], "trigger_grave_passive")
+  }
 }
