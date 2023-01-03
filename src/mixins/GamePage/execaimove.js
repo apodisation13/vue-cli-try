07c15caf75fd21618899a7057367da96c46c5c7d
james <apodisation13@gmail.com>
2023-01-03 20:58:09 +0300

    enemy passive ability in line with other logic

--- src/mixins/GamePage/execaimove.js
+++ src/mixins/GamePage/execaimove.js
@@ -1,9 +1,6 @@
 import { player_passive_abilities_end_turn } from "@/logic/player_move/player_passive_abilities"
 import { ai_move } from "@/logic/ai_move/ai_move"
-import {
-  enemy_passive_abilities_end_turn,
-  passive_leader_ai_move,
-} from "@/logic/ai_move/ai_passive_abilties"
+import { enemy_passive_abilities_end_turn } from "@/logic/ai_move/ai_passive_abilties"
 import { appear_new_enemy } from "@/logic/game_logic/place_enemies"
 
 export default {
@@ -13,10 +10,9 @@ export default {
       this.$store.commit("set_player_turn", false) // кнопка пас сразу пропала и дро тоже
 
       // А ДАЛЬШЕ последовательно выполняются
-      // - пассивки карт игрока
+      // - пассивки карт игрока + пассивка лидера игрока
       // - ход компа
-      // - пассивка лидера врагов
-      // - пассивки самих врагов
+      // - пассивки самих врагов + пассивка лидера врагов
       // - появление нового врага
       // - переход хода снова игроку
 
@@ -30,28 +26,20 @@ export default {
 
           let await_ai_move = setInterval(() => {
             if (!this.$store.state.game.ai_move) {
-              console.log("закончили ходить комп, ходит лидер врагов")
+              console.log("закончили ходить комп, теперь пассивки врагов")
               clearInterval(await_ai_move)
-              passive_leader_ai_move(this.gameObj.enemy_leader)
+              enemy_passive_abilities_end_turn(this.gameObj)
 
-              let await_leader_ai_passive_move = setInterval(() => {
-                if (!this.$store.state.game.leader_ai_move) {
-                  console.log("закончили лидер врагов, теперь пассивки врагов")
-                  clearInterval(await_leader_ai_passive_move)
-                  enemy_passive_abilities_end_turn(this.gameObj)
-
-                  let await_epa_end_turn = setInterval(() => {
-                    if (!this.$store.state.game.epa_end_turn) {
-                      console.log(
-                        "всё закончили, щас появится новый враг и можно ходить снова"
-                      )
-                      clearInterval(await_epa_end_turn)
-                      appear_new_enemy(this.gameObj.field, this.gameObj.enemies)
-                      this.isActive.player_cards = true
-                      this.can_draw = this.calc_can_draw()
-                      this.$store.commit("set_player_turn", true)
-                    }
-                  }, 500)
+              let await_epa_end_turn = setInterval(() => {
+                if (!this.$store.state.game.epa_end_turn) {
+                  console.log(
+                    "всё закончили, щас появится новый враг и можно ходить снова"
+                  )
+                  clearInterval(await_epa_end_turn)
+                  appear_new_enemy(this.gameObj.field, this.gameObj.enemies)
+                  this.isActive.player_cards = true
+                  this.can_draw = this.calc_can_draw()
+                  this.$store.commit("set_player_turn", true)
                 }
               }, 500)
             }
